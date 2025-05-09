b797e14e54857fcf413d938496ab333a5ffe3a5a
ibilyk91 <ivan.bilyk95@myhunter.cuny.edu>
2025-05-09 14:30:57 -0400

    test: complete 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  for (let j = 0; j < remove.length; j++) {  // loops items in ...remove array
+    let i = 0;
+    while (i < array.length) {
+      if (array[i] === remove[j]) {
+        array.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  return roundToOneDecimal((degree - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  return roundToOneDecimal((degree * 9 / 5) + 32);
 };
+
+function roundToOneDecimal(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
